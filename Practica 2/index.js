0300a1cb37cd85e70fd8c879036885460536eddb
Abraham Ramirez <abrahammoreno044@gmail.com>
2022-02-12 13:05:00 -0600

    Agregado javascript

---
+++ Practica 2/index.js
@@ -0,0 +1,11 @@
+
+var n = 0
+var parrAdv = document.getElementById("advertencia")
+var x = "Te dije que no"
+
+function tocoLoQueNo() {
+  n = n+1
+  console.log("Toco veces: ")
+  console.log(n)
+  parrAdv.innerHTML = "TE DIJE QUE NO ME TOQUES"
+}
