71a1379c26a23b55f68732190e0243984e129907
Muyraro23 <test123>
2022-02-19 14:48:24 -0600

    P1: usado DOM en vez de Texto para App

--- practica1/index.js
+++ practica1/index.js
@@ -7,7 +7,7 @@ var montoTxt = document.getElementById("monto")
 var totalMsg = document.getElementById("totalMsg")
 var recibo = document.getElementById("recibo")
 
-function CalcularTotal() {
+function AgregarEl() {
   //Obtiene el valor del monto
   monto = montoTxt.value
   monto = parseInt(monto)
@@ -15,10 +15,19 @@ function CalcularTotal() {
   //Lo suma al total
   total = total + monto
 
-  //Agrega Row con las 2 columnas
-  recibo.innerHTML = recibo.innerHTML + 
-    "<ion-row><ion-col size='6'>" + descTxt.value + "</ion-col>" + 
-    "<ion-col size='6'> $" + montoTxt.value + "</ion-col></ion-row>"
+//Lo agrega a la tabla
+  
+  nuevarow = document.createElement("ion-row") //Crea row
+  nuevadesc = document.createElement("ion-col") //Crea columna de desc
+  nuevoprec = document.createElement("ion-col") //Crea columna de precio
+
+  nuevadesc.innerText = descTxt.value //Asigna valor a la descripcion
+  nuevoprec.innerText = "$" + montoTxt.value //Asigna valor al precio
+
+  nuevarow.appendChild(nuevadesc) //Agrega la columna de desc al row
+  nuevarow.appendChild(nuevoprec) //Agrega la columna del precio al row
+
+  recibo.appendChild(nuevarow)  //Agrega el row a la tabla
 
   //Cambia valor del total
   totalMsg.innerHTML = "Total: $" + total
